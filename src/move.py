974656c71aac4f3c10403ea94f5ac48222e800b1
Ivan Zhong <s1904845@creosote.inf.ed.ac.uk>
2022-02-08 18:56:03 +0000

    Added square code

--- src/move.py
+++ src/move.py
@@ -19,10 +19,14 @@ class free_space_navigation():
         self.turtlebot_odom_pose = Odometry()
         self.velocityPublisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.pose_subscriber = rospy.Subscriber("/odom", Odometry, self.poseCallback)
-        movelength = 0.1
-        maximum_speed = 0.01
+        movelength = 0.5
+        maximum_speed = 0.025
+        for i in range(0,4):
+            self.move_v1(maximum_speed, movelength, True)
+            self.rotate(0.5, self.degree2radian(90.0),True)
         # self.move_v1(maximum_speed, movelength, True)
-        self.rotate(0.2, self.degree2radian(90.0),True)
+        # self.rotate(0.5, self.degree2radian(90.0),True)
+        # self.move_v1(maximum_speed, movelength, True)
         rospy.loginfo("All step completed. Trying auto termination")
         raise rospy.ROSInterruptException
 
@@ -77,11 +81,8 @@ class free_space_navigation():
     def rotate(self,angular_velocity,radians,clockwise):
         rotateMessage = Twist()
         listener = tf.TransformListener()
-        #init_transform: is the transformation before starting the motion
         init_transform = geometry_msgs.msg.TransformStamped()
-        #current_transformation: is the transformation while the robot is moving
         current_transform = geometry_msgs.msg.TransformStamped()
-        
         angle_turned = 0.0
 
         angular_velocity = (-angular_velocity, 0.2)[angular_velocity > 0.2]
@@ -91,11 +92,8 @@ class free_space_navigation():
 
         while(radians > 2* pi):
             radians -= 2* pi
-        
         listener.waitForTransform("/base_footprint", "/odom", rospy.Time(0), rospy.Duration(10.0) )
         (trans,rot) = listener.lookupTransform('/base_footprint', '/odom', rospy.Time(0))
-        #listener.lookupTransform("/base_footprint", "/odom", rospy.Time(0),init_transform)
-        
         init_transform.transform.translation = trans
         init_transform.transform.rotation =rot
 
@@ -116,34 +114,21 @@ class free_space_navigation():
         loop_rate = rospy.Rate(20)
         
         while True:
-            rospy.loginfo("Turtlebot is Rotating")
-
             self.velocityPublisher.publish(rotateMessage)
-         
             loop_rate.sleep()
-                    
-            #rospy.Duration(1.0)
-
             try:
-
-                #wait for the transform to be found
                 listener.waitForTransform("/base_footprint", "/odom", rospy.Time(0), rospy.Duration(10.0) )
-                #Once the transform is found,get the initial_transform transformation.
-                #listener.lookupTransform("/base_footprint", "/odom",rospy.Time(0))
                 (trans,rot) = listener.lookupTransform('/base_footprint', '/odom', rospy.Time(0))
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 rospy.Duration(1.0)
-
             current_transform.transform.translation = trans
             current_transform.transform.rotation =rot
-
             #since the rotation is only in the Z-axes 
             #end_angle = 0.5 * sqrt( rot[2] ** 2)
             euler = tf.transformations.euler_from_quaternion(rot)
             roll = euler[0]
             pitch = euler[1]
             end_angle = euler[2]
-            
             angle_turned = abs(end_angle - start_angle)
             print("angle_turned: %s" %angle_turned)
             if (angle_turned > radians):
