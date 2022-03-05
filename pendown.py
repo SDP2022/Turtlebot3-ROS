abe247ada7418c081b35e1d63f2e554b75859a52
Tianjiao Wang <73406569+TJWRii@users.noreply.github.com>
2022-03-05 21:27:21 +0000

    Add files via upload

---
+++ pendown.py
@@ -0,0 +1,17 @@
+from motors import Motors 
+from time import time, sleep 
+
+mc = Motors() 
+ 
+motor_id = 2        # motor port
+speed = -100         # forward = positive, backwards = negative 
+run_time = 2        # number of seconds to run motors 
+
+ 
+# Move motor with the given ID at your set speed 
+mc.move_motor(motor_id,speed)      
+start_time = time() 
+
+sleep(0.5)     # avoid errors 
+ 
+mc.stop_motors() 
